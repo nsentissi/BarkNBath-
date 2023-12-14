3fc5eab6519d1b33696c3cca1f73445a3a469e66
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2023-12-14 10:55:22 +0100

    first commit: initiliazing project

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const port = 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
