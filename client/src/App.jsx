2366c7647c36fcc687e2c48a77236e4ab39af7be
bishaldiganta <bishal.diganta@gmail.com>
2023-12-22 17:38:13 +0100

    added aboutus section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Journey from './components/Journey'
 import Login from './components/Login'
 import Signup from './components/Signup'
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Signup/>
       <Login/>
+      <Journey/>
     </>
   )
 }
