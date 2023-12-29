1b0cf5d10839ae79d936519843c8d299c11c2b60
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2023-12-29 15:11:51 +0100

    merged two sections together

--- client/src/components/Journey.jsx
+++ client/src/components/Journey.jsx
@@ -1,6 +1,6 @@
 const Journey = () => {
   return (
-    <div className=" bg-white pt-48 justify-center px-32 ">
+    <div className=" bg-white pt-48 justify-center ">
       <div className="flex flex-col justify-center text-center">
         <h2 className="text-2xl font-extrabold pb-8">How it works</h2>
         <p className="pb-8">
@@ -23,8 +23,8 @@ const Journey = () => {
           
         </div>
       </div>
-      <div className="flex justify-center bg-gray-100 mx-auto rounded-t-3xl gap-8 p-8">
-        <h2 className="text-2xl font-extrabold text-center">About Us</h2>
+      <div className=" flex flex-col md:flex-row justify-center bg-red-100 mx-auto rounded-t-3xl lg:mx-32 p-8 gap-8 mx-4 md:mx-8 ">
+        <h2 className="text-2xl font-extrabold text-center ">About Us</h2>
         <p className="text-center">
           "Bark N Bath is dedicated to providing top-notch dog grooming and
           cleaning services. Our passionate team, skilled in the latest grooming
