9e9f00a4665c8423fa6d8ae9d6c8d511ba812c08
bishaldiganta <bishal.diganta@gmail.com>
2023-12-30 09:46:35 +0100

    testing component shared by Nawfal

--- client/src/components/Journey.jsx
+++ client/src/components/Journey.jsx
@@ -7,23 +7,8 @@ const Journey = () => {
           ShakeYourTail is an online scheduling and CRM service & is super easy
           to use, you can trial our service free for 30 days.
         </p>
-        <div className="flex justify-center gap-36 bg-[url('/../client/src/assets/dotted-path.svg')] h-40 bg-no-repeat bg-contain">
-          <div class="w-20 h-20 rounded-full bg-gray-200 text-gray-700 ">
-            1
-          </div>
-          <div class="w-20 h-20 rounded-full inline-flex items-center justify-center  bg-gray-200 text-gray-700 z-50">
-            2
-          </div>
-          <div class="w-20 h-20 rounded-full bg-gray-200 justify- text-gray-700">
-            3
-          </div>
-          <div class="w-20 h-20 rounded-full inline-flex items-center justify-center  bg-gray-200 text-gray-700">
-            4
-          </div>
-          
-        </div>
       </div>
-      <div className=" flex flex-col md:flex-row justify-center bg-red-100 mx-auto rounded-t-3xl lg:mx-32 p-8 gap-8 mx-4 md:mx-8 ">
+      <div className=" flex flex-col md:flex-row justify-center bg-red-100 rounded-t-3xl lg:mx-32 p-8 gap-8 mx-4 md:mx-8 ">
         <h2 className="text-2xl font-extrabold text-center ">About Us</h2>
         <p className="text-center">
           "Bark N Bath is dedicated to providing top-notch dog grooming and
