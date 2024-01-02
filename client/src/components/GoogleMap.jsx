9056332906eaeb6cf11995b4cba78bbeadbfe591
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2024-01-02 16:17:51 +0100

    added pins on map

--- client/src/components/GoogleMap.jsx
+++ client/src/components/GoogleMap.jsx
@@ -1,26 +1,11 @@
-import React from "react";
-import GoogleMapReact from "google-map-react";
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import mapGeoJson from '../data/map.json'
 
-const AnyReactComponent = ({ text }) => (
-    <div style={{
-      color: 'white', 
-      background: 'grey',
-      padding: '10px',
-      display: 'inline-flex',
-      textAlign: 'center',
-      alignItems: 'center',
-      justifyContent: 'center',
-      borderRadius: '100%',
-      transform: 'translate(-50%, -50%)'
-    }}>
-      {text}
-    </div>
-  );
+
 
 const GoogleMap = () => {
- 
-  
-    const defaultProps = {
+  const defaultProps = {
     center: {
       lat: 52.5200,
       lng: 13.4050,
@@ -28,26 +13,27 @@ const GoogleMap = () => {
     zoom: 12,
   };
 
-  const locations = [
-    { lat: 52.32, lng: 13.23, text: "Location 1" },
-    { lat: -34.4, lng: 150.65, text: "Location 2" },
-  ];
+  const handleApiLoaded = ({ map, maps }) => {
+    
+    map.data.addGeoJson(mapGeoJson);
+    
+    
+    map.data.setStyle({
+      fillColor: 'green',
+      strokeWeight: 2,
+    });
+  };
 
   return (
-    <div className="w-full h-96">
+    <div style={{ height: '500px', width: '100%' }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyDSSyR_KZ6-aQSbVJuZAnEU4Prs8atayTk" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={handleApiLoaded}
       >
-        {locations.map((location, index) => (
-          <AnyReactComponent
-            key={index}
-            lat={location.lat}
-            lng={location.lng}
-            text={location.text}
-          />
-        ))}
+
       </GoogleMapReact>
     </div>
   );
