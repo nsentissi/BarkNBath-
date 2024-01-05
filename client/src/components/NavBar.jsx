104837f96e7043e30dc3faad3994cf7bed2ada1f
Nawfal Sentissi <nadjanawfal@Nawfals-Air.fritz.box>
2024-01-05 12:24:11 +0100

    worked on the modal implementation

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import { useAuth } from '../hooks/AuthContext';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useAuth } from "../hooks/AuthContext";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ onProfileClick }) => {
   const { currentUser, logout } = useAuth();
 
   return (
-    <nav className="bg-white dark:bg-transparent fixed w-full z-20 top-0 start-0 ">
+    <nav className="bg-transparent dark:bg-transparent fixed w-full z-20 top-0 start-0 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/homepage" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src=".\src\assets\barkNBath.webp" className="h-20 w-24" alt="barknbath Logo" />
-          
+        <Link
+          to="/homepage"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src=".\src\assets\barkNBath.webp"
+            className="h-20 w-24"
+            alt="barknbath Logo"
+          />
         </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
             type="button"
             className="text-black  bg-info hover:bg-info focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-info dark:hover:bg-accent "
           >
-           {currentUser ? (
+            {currentUser ? (
               <div>
-                <Link to="/profile" className="mr-2">
+                <button onClick={onProfileClick}>
                   Welcome, {currentUser.firstName}
-                </Link>
+                </button>
                 <button onClick={logout}>Logout</button>
               </div>
             ) : (
@@ -34,13 +40,19 @@ const Navbar = () => {
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-sticky"
             aria-expanded="false"
-          >
-          </button>
+          ></button>
         </div>
-        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
             <li>
-              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                aria-current="page"
+              >
                 Home
               </a>
             </li>
@@ -60,9 +72,6 @@ const Navbar = () => {
                 About us
               </a>
             </li>
-             
-           
-            
           </ul>
         </div>
       </div>
