3684e22f331a4535090b58dc2ac20653956df690
unknown <xhenikullafi06@gmail.com>
2024-01-05 13:24:45 +0100

    once the user is logged in the navbar changes and also the content on the landingpage

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,32 +1,40 @@
-import React from 'react'
-import AboutUs from './AboutUs'
-import Services from './Services'
-import Landingpage from './Landingpage'
-import Navbar from './Navbar'
-import Journey from './Journey'
-import Footer from './Footer'
-import Faqsection from './Faqsection'
-import GoogleMap from './GoogleMap'
-import Testimonials from './Testimonials'
-import ServicesTable from './ServicesTable'
-import Carousel from './Carousel'
+import React from 'react';
+import { useAuth } from '../hooks/AuthContext'; 
+
+import AboutUs from './AboutUs';
+import Services from './Services';
+import Landingpage from './Landingpage';
+import Navbar from './NavBar';
+import Journey from './Journey';
+import Footer from './Footer';
+import Faqsection from './Faqsection';
+import GoogleMap from './GoogleMap';
+import Testimonials from './Testimonials';
+import ServicesTable from './ServicesTable';
+import Carousel from './Carousel';
 
 const Homepage = () => {
+  const { currentUser } = useAuth(); 
+
   return (
     <div>
-      <Navbar/>
-      <Landingpage/>
-      <Journey/>
-      <AboutUs/>
-      <Services/>
-      <GoogleMap/>
-      <Carousel/>
-      <Testimonials/>
-      {/* <ServicesTable/> */}
-      <Faqsection/>
-      <Footer/>
+      <Navbar />
+      {!currentUser && (
+        <>
+          <Landingpage />
+          <Journey />
+          <AboutUs />
+          <Services />
+          <GoogleMap />
+          <Carousel />
+          <Testimonials />
+          {/* <ServicesTable/> */}
+          <Faqsection />
+          <Footer />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
