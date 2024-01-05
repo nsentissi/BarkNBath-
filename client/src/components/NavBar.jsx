f59cc3dc205ee0c2fa1061f9fa0e896f156ddb33
unknown <xhenikullafi06@gmail.com>
2024-01-05 18:15:23 +0100

    navbar responsivnes last touches

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -25,10 +25,17 @@ const Navbar = ({ onProfileClick }) => {
           {currentUser ? (
             <React.Fragment>
               {/* Desktop Links for Logged-in User */}
+             
+              <span className="mr-4 hidden md:block">Welcome, {currentUser.firstName}</span>
+              <Link
+                          to="/add-pet"
+                          className="mr-4 hidden md:block"
+                        >
+                          Add your puffy friend
+                        </Link>
               <button onClick={onProfileClick} className="mr-4 hidden md:block">
                 Your account
               </button>
-              <span className="mr-4 hidden md:block">Welcome, {currentUser.firstName}</span>
               <button className="font-sans hidden md:block" onClick={logout}>
                 Logout
               </button>
@@ -88,7 +95,7 @@ const Navbar = ({ onProfileClick }) => {
                   </div>
                 )}
               </div>
-              {/* Link to log in for Logged-out User */}
+              {/* Link to log in */}
               <Link className="font-sans ml-4 md:block" to="/login">
                 Log in
               </Link>
@@ -96,7 +103,7 @@ const Navbar = ({ onProfileClick }) => {
           )}
         </div>
 
-        {/* Desktop Menu - Hidden when logged in */}
+        {/* menu - Hidden when logged in */}
         {!currentUser && (
           <div className="hidden md:block">
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-transparent md:dark:bg-transparent ">
