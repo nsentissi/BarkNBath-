bee09588f6cf52d5f6a6891357bec1c82f335ea8
unknown <xhenikullafi06@gmail.com>
2024-01-05 10:19:12 +0100

    landing page

--- client/src/components/Landingpage.jsx
+++ client/src/components/Landingpage.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const Landingpage = () => {
-  const gifVariants = {
-    hidden: { opacity: 0, x: -100 },
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: { type: "spring", stiffness: 80, duration: 1, delay: 1 },
-    },
-  };
-
   const textVariants = {
     hidden: { opacity: 0, x: 100 },
     visible: {
@@ -23,43 +14,51 @@ const Landingpage = () => {
   const vanVariants = {
     hidden: { x: "100%" },
     visible: {
+      opacity: 1,
       x: 0,
-      transition: { type: "tween", duration: 3, delay: 1 },
+      transition: { type: "spring", duration: 3, delay: 1, bounce: 0.2 },
     },
   };
 
   return (
-    <div style={{backgroundImage: `url('./src/assets/homepage.svg')`, backgroundSize: 'cover', backgroundPosition: 'center center', height: '100vh'}}>
+    <div
+      style={{
+        backgroundImage: `url('./src/assets/homepage.svg')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        height: "100vh",
+      }}
+    >
       <motion.div
-        className="flex flex-row justify-center items-center h-screen"
+        className="flex flex-col items-center justify-center"
+        variants={textVariants}
+        style={{ position: "absolute", bottom: 380, right: 400 }}
         initial="hidden"
         animate="visible"
       >
-        <motion.div
-           className="flex flex-col items-center justify-center mr-32" 
-          variants={gifVariants}
-          style={{ width: '25%' }}
-        >
-          <motion.img
-            
-            src=".\src\assets\dogintub.gif"
-            alt="Dog in Tub"
-          />
-        </motion.div>
-
-        <motion.div
-          className="flex justify-end"
-          variants={textVariants}
-        >
-          <h1 className="text-6xl text-white font-chewy">Pamper Your Dog </h1>
-        </motion.div>
+        <h1 className="text-6xl text-white font-chewy">Pamper Your Dog </h1>
       </motion.div>
 
       <motion.img
         src="./src/assets/van.png"
         alt="Van"
-        style={{ position: 'absolute', bottom: 0, right: 0 }}
+        style={{ position: "absolute", bottom: 0, right: 0 }}
         variants={vanVariants}
+        initial="hidden"
+        animate="visible"
+      />
+      <motion.img
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          borderRadius: "50%",
+          overflow: "hidden",
+          position: "absolute",
+          bottom: 370,
+          right: 300,
+        }}
+        src="./src/assets/parkingspace.png"
+        alt="Van"
+        variants={textVariants}
         initial="hidden"
         animate="visible"
       />
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,51 +1,73 @@
-import React from "react";
-import { useAuth } from "../hooks/AuthContext";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useAuth } from '../hooks/AuthContext';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+const Navbar = () => {
+  const { currentUser, logout } = useAuth();
 
-  const { currentUser, logout } = useAuth(); 
   return (
-    <div className="navbar fixed flex-row z-50 bg-transparent">
-      <div className="flex-1">
-        <img  className="w-48" style={{ boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.3)' }} src="../src/assets/barkNBath.webp"></img>
-      </div>
-      <div className="flex-none text-indigo-50">
-        <ul className="menu menu-horizontal px-1">
-        <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>About us</a>
-          </li>
-          <li>
-            
-            <details>
-              <summary>Our services</summary>
-              <ul className="p-2 bg-base-100 rounded-t-none text-black">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-              
-            </details>
-            
-          </li>
-          {currentUser ? (
-        <div>
-          <Link to="/profile" className="mr-2">Welcome, {currentUser.firstName}</Link>
-          <button onClick={logout}>Logout</button>
+    <nav className="bg-white dark:bg-transparent fixed w-full z-20 top-0 start-0 ">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/homepage" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src=".\src\assets\barkNBath.webp" className="h-20 w-24" alt="barknbath Logo" />
+          
+        </Link>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="text-black  bg-info hover:bg-info focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-info dark:hover:bg-accent "
+          >
+           {currentUser ? (
+              <div>
+                <Link to="/profile" className="mr-2">
+                  Welcome, {currentUser.firstName}
+                </Link>
+                <button onClick={logout}>Logout</button>
+              </div>
+            ) : (
+              <Link to="/login">Log in</Link>
+            )}
+          </button>
+          <button
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+          </button>
         </div>
-      ) : (
-        <Link to="/login" >Login</Link>
-      )}
-        </ul>
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-transparent md:dark:bg-transparent dark:border-gray-700">
+            <li>
+              <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                About us
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                About us
+              </a>
+            </li>
+             
+           
+            
+          </ul>
+        </div>
       </div>
-    </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
