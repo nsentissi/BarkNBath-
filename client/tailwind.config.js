727e993f3586e99640e76dbd3d5673a9cc3fbe31
bishaldiganta <bishal.diganta@gmail.com>
2024-01-06 21:09:17 +0100

    installed shadcn for testing

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,45 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-
-
-
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      fontFamily: {
-        chewy: ['Chewy', 'cursive'],
-        dosis: ['Dosis', 'sans-serif'],
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-
-
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-
-          primary: "#19759f",
-
-          secondary: "#168aad",
-
-          accent: "#52b69a",
-
-          neutral: "#ffc500",
-
-          /* "base-100": "#40CCD4", */
-
-          info: "#F5C07A",
-
-          success: "#D4E8B0",
-
-          warning: "#ffffff",
-
-          error: "#ffffff",
-
-        },
-      },
-    ],
-  },
-  plugins: [require("daisyui")],
-};
+  plugins: [require("tailwindcss-animate")],
+}
