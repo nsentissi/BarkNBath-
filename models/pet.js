ff153a1714f7d282c12f63d5ca377cbf1f36e970
Nawfal Sentissi <nadjanawfal@Nawfals-MacBook-Air.local>
2024-01-08 15:09:13 +0100

    fix conflicts

--- models/pet.js
+++ models/pet.js
@@ -3,12 +3,11 @@ const { Schema, model } = require("mongoose");
 const petSchema = new Schema({
 
   owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
-
   name: { type: String, required: true },
   breed: { type: String, required: true },
   age: { type: Number, required: true },
   weight: { type: Number, required: true },
- /*  appointments: [{ type: Schema.Types.ObjectId, ref: "Appointment" }], */
+  appointments: [{ type: Schema.Types.ObjectId, ref: "Appointment" }], 
   Bio: { type: String, required: true },
   profilePhoto: { type: Buffer },
 });
