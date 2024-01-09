47541208f5da7dc781373e88b98efef6aa39e604
unknown <xhenikullafi06@gmail.com>
2024-01-09 13:39:41 +0100

    add changes on the services section

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-
+import { useInView } from "react-intersection-observer";
 function Services() {
   const cardVariants = {
     offscreen: { y: 50, opacity: 0 },
@@ -14,6 +14,20 @@ function Services() {
       },
     },
   };
+  const [card1Ref, card1InView] = useInView();
+  const [card2Ref, card2InView] = useInView();
+  const [card3Ref, card3InView] = useInView();
+  const [card4Ref, card4InView] = useInView();
+  const [card5Ref, card5InView] = useInView();
+  const [card6Ref, card6InView] = useInView();
+
+  const cardHoverVariants = {
+    hover: {
+      scale: 1.05,
+      boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+    },
+  };
+
 
   return (
     <div className="py-20">
@@ -28,7 +42,13 @@ function Services() {
         id="Projects"
         class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
       >
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        <motion.div
+          ref={card1Ref}
+          variants={cardVariants}
+          initial="offscreen"
+          animate={card1InView ? "onscreen" : "offscreen"}
+          className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+        >
           <a href="#">
             <img
               src="https://images.unsplash.com/photo-1646753522408-077ef9839300?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
@@ -66,9 +86,14 @@ function Services() {
               </p>
             </div>
           </a>
-        </div>
+        </motion.div>
 
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        <motion.div
+          ref={card2Ref}
+          variants={cardVariants}
+          initial="offscreen"
+          animate={card2InView ? "onscreen" : "offscreen"}
+          className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
           <a href="#">
             <img
               src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
@@ -101,13 +126,19 @@ function Services() {
                 </div>
               </div>
               <p class="text-sm font-semibold text-black cursor-auto my-3">
-                  A perfect trim for a perfect pup!
-                </p>
+                A perfect trim for a perfect pup!
+              </p>
             </div>
           </a>
-        </div>
+          </motion.div>
 
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+          <motion.div
+          ref={card3Ref}
+          variants={cardVariants}
+          initial="offscreen"
+          animate={card3InView ? "onscreen" : "offscreen"}
+          className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+        >
           <a href="#">
             <img
               src="https://images.unsplash.com/photo-1651950537598-373e4358d320?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
@@ -116,38 +147,43 @@ function Services() {
             />
             <div class="px-4 py-3 w-72">
               <span class="text-gray-400 mr-3 uppercase text-xs">Package</span>
-              
+
               <div class="flex items-center">
-              <p class="text-lg font-bold text-black truncate block capitalize">
-                Ultimate Paw Care
+                <p class="text-lg font-bold text-black truncate block capitalize">
+                  Ultimate Paw Care
+                </p>
+
+                <div class="ml-auto">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    class="bi bi-bag-plus"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                    />
+                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                  </svg>
+                </div>
+              </div>
+              <p class="text-sm font-semibold text-black cursor-auto my-3">
+                Pad Shaving, Nail Clipping, Feet Scissoring and More!
               </p>
-                
-
-                <div class="ml-auto">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    class="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
-                </div>
-              </div>
-              <p class="text-sm font-semibold text-black cursor-auto my-3">
-                  Pad Shaving, Nail Clipping, Feet Scissoring and More!
-                </p>
             </div>
           </a>
-        </div>
+        </motion.div>
 
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        <motion.div
+          ref={card4Ref}
+          variants={cardVariants}
+          initial="offscreen"
+          animate={card4InView ? "onscreen" : "offscreen"}
+          className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+        >
           <a href="#">
             <img
               src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
@@ -159,7 +195,7 @@ function Services() {
 
               <div class="flex items-center">
                 <p class="text-lg font-bold text-black truncate block capitalize">
-                The Works
+                  The Works
                 </p>
 
                 <div class="ml-auto">
@@ -180,13 +216,19 @@ function Services() {
                 </div>
               </div>
               <p class="text-sm font-semibold text-black cursor-auto my-3">
-              De-shedding, Brushing & Hand Blow Drying
-                </p>
+                De-shedding, Brushing & Hand Blow Drying
+              </p>
             </div>
           </a>
-        </div>
+        </motion.div>
 
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        <motion.div
+          ref={card5Ref}
+          variants={cardVariants}
+          initial="offscreen"
+          animate={card5InView ? "onscreen" : "offscreen"}
+          className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl"
+        >
           <a href="#">
             <img
               src="https://images.unsplash.com/photo-1651950519238-15835722f8bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mjh8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
@@ -198,7 +240,7 @@ function Services() {
 
               <div class="flex items-center">
                 <p class="text-lg font-bold text-black truncate block capitalize">
-                Doggy Dental Brushing
+                  Doggy Dental Brushing
                 </p>
 
                 <div class="ml-auto">
@@ -219,13 +261,22 @@ function Services() {
                 </div>
               </div>
               <p class="text-sm font-semibold text-black cursor-auto my-3">
-              Keep Your Dog's K-9's clean and strong with a thorough brushing and fresh breath spray
-                </p>
+                Keep Your Dog's K-9's clean and strong with a thorough brushing
+                and fresh breath spray
+              </p>
             </div>
           </a>
-        </div>
+        </motion.div>
 
-        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+        <motion.div
+          ref={card6Ref}
+          variants={cardVariants}
+          initial="offscreen"
+          
+          whileHover="hover"
+          animate={card6InView ? "onscreen" : "offscreen"}
+          className="w-72 bg-white shadow-md rounded-xl duration-500"
+        >
           <a href="#">
             <img
               src="https://images.unsplash.com/photo-1649261191606-cb2496e97eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
@@ -234,12 +285,12 @@ function Services() {
             />
             <div class="px-4 py-3 w-72">
               <span class="text-gray-400 mr-3 uppercase text-xs">Package</span>
-             
+
               <div class="flex items-center">
-              <p class="text-lg font-bold text-black truncate block capitalize">
-              Breed Standard
-              </p>
-                
+                <p class="text-lg font-bold text-black truncate block capitalize">
+                  Breed Standard
+                </p>
+
                 <div class="ml-auto">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -257,11 +308,12 @@ function Services() {
                   </svg>
                 </div>
               </div>
-              <p class="text-sm font-semibold text-black cursor-auto my-3">Full Service Styling According To Your Breed - Show Ready</p>
-
+              <p class="text-sm font-semibold text-black cursor-auto my-3">
+                Full Service Styling According To Your Breed - Show Ready
+              </p>
             </div>
           </a>
-        </div>
+        </motion.div>
       </section>
     </div>
   );
