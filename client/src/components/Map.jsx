713457ff9f5be69a5af2c081b51b9e00d9bf4521
unknown <xhenikullafi06@gmail.com>
2024-01-10 13:59:29 +0100

    styling on the map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -8,10 +8,10 @@ const position = [52.5314560163944, 13.403370226299785];
 const Map = () => {
   return (
     <>
-    <div className="bg-[url('../../src/assets/map-background.png')] bg-cover bg-center h-auto md:h-100 bg-no-repeat">
-      <div className=" w-3/4 mx-auto flex flex-col md:flex-row justify-end px-16 py-14 gap-4  h-auto md:h-100 w-full ">
-        <div className=" md:w-2/4 lg:w-2/5 px-4 py-10 md:p-12 bg-success h-1/4 lg rounded-lg">
-          <h4 className="text-base md:text-lg lg:text-xl w-full md:w-2/4 lg:w-3/4 p-4 md:p-20">
+      <div className="bg-[url('../../src/assets/map-background.png')] bg-cover bg-center h-auto md:h-full bg-no-repeat">
+      <div className="mx-auto flex flex-col md:flex-row justify-center px-4 py-14 gap-4 h-auto md:h-full">
+        <div className="px-4 py-10 md:p-12 bg-success h-1/4 lg rounded-lg">
+          <h4 className="text-base md:text-lg lg:text-xl w-full p-4 md:p-20">
             Explore our locations
           </h4>
         </div>
@@ -19,7 +19,7 @@ const Map = () => {
           center={position}
           zoom={12}
           scrollWheelZoom={false}
-          className="w-full md:w-2/4 lg:w-3/5 h-96 rounded-lg shadow-lg flex-col"
+          className="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 h-96 rounded-lg shadow-lg"
         >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
