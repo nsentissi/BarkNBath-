bdbf89b6fad8f132bdcd3ada5bec21f7cef89a83
unknown <xhenikullafi06@gmail.com>
2024-01-18 09:40:54 +0100

    last push

--- client/src/components/Dashboard/PetList.jsx
+++ client/src/components/Dashboard/PetList.jsx
@@ -9,7 +9,13 @@ import trashapp from "../../assets/trashapp.svg";
 
 const PetList = () => {
   const { currentUser } = useAuth();
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [appointmentToDelete, setAppointmentToDelete] = useState(null);
 
+  const openDeleteModal = (appointmentId) => {
+    setAppointmentToDelete(appointmentId);
+    setShowDeleteModal(true);
+  };
   const navigate = useNavigate();
   const [petAppointments, setPetAppointments] = useState([]);
 
@@ -37,8 +43,20 @@ const PetList = () => {
       console.error("Failed to delete the appointment:", error);
     }
   };
-
+  const deleteAppointment = async () => {
+    if (appointmentToDelete) {
+      try {
+        await handleDeleteAppointment(appointmentToDelete);
+        setShowDeleteModal(false);
+        console.log("Appointment deleted successfully");
+      } catch (error) {
+        console.error("Failed to delete the appointment:", error);
+      }
+    }
+  };
+  
   useEffect(() => {
+   
     const fetchAppointments = async () => {
       if (!currentUser || !currentUser.pets) return;
 
@@ -173,8 +191,7 @@ const PetList = () => {
                         </p>
                       </div>
                       <button
-                        onClick={() =>
-                          handleDeleteAppointment(appointment._id, pet._id)
+                        onClick={() => openDeleteModal(appointment._id, pet._id)
                         }
                         className="text-black"
                       >
@@ -220,6 +237,43 @@ const PetList = () => {
             </div>
           </div>
         ))}
+        {showDeleteModal && (
+            <>
+              <div
+                className="fixed inset-0 bg-black bg-opacity-30 z-40"
+                style={{ backdropFilter: "blur(5px)" }}
+              ></div>
+              <div
+                id="popup-modal"
+                tabIndex="-1"
+                className="overflow-y-auto overflow-x-hidden fixed inset-0 z-50 flex justify-center items-center"
+              >
+                <div className="relative p-4 w-full max-w-md max-h-full">
+                  <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <div className="p-4 md:p-5 text-center">
+                      <h3 className="mb-5 text-lg font-normal text-white dark:text-white">
+                        Are you sure you want to delete this appointment?
+                      </h3>
+                      <button
+                        onClick={deleteAppointment}
+                        type="button"
+                        className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2"
+                      >
+                        Yes, I'm sure
+                      </button>
+                      <button
+                        onClick={() => setShowDeleteModal(false)}
+                        type="button"
+                        className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                      >
+                        No, cancel
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
       </div>
     </>
   );
